f221185c4cae77a86adab45e2d41010cee699a84
IsaDelPortico <167131113+IsaDelPortico@users.noreply.github.com>
2024-04-25 13:14:53 -0600

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -1,7 +1,4 @@
-URL_SERVICE = "https://cnt-f6308937-0cfb-46ec-8602-e644b757a2eb.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-cdef3f71-4297-41ff-a579-75f74741e999.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
-LOG_MAIN_PATH = "/api/logs/main/"
-USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
 CREATE_USER_PATH = "/api/v1/users/"
-PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
 CREATE_KIT_URL = "/api/v1/kits"
